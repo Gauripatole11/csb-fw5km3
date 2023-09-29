94e351db1a38830e56f268dee67363d0537abb38
Code Sandbox <hello@codesandbox.io>
2023-09-29 19:12:40 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.341f11ec0f04ade78db2a182e30f7351.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cbd311fee68c6d58976aedafd870db6",
+    "url": "https://gauripatole11.github.io/csb-fw5km3/index.html"
+  },
+  {
+    "revision": "f4bf00a7e318c9469ba1",
+    "url": "https://gauripatole11.github.io/csb-fw5km3/static/css/2.bb24780d.chunk.css"
+  },
+  {
+    "revision": "1f71382255766702a45e",
+    "url": "https://gauripatole11.github.io/csb-fw5km3/static/css/main.9b1431d8.chunk.css"
+  },
+  {
+    "revision": "f4bf00a7e318c9469ba1",
+    "url": "https://gauripatole11.github.io/csb-fw5km3/static/js/2.20a08c70.chunk.js"
+  },
+  {
+    "revision": "847c3d0a59616cf5ab5705d4dde8665f",
+    "url": "https://gauripatole11.github.io/csb-fw5km3/static/js/2.20a08c70.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f71382255766702a45e",
+    "url": "https://gauripatole11.github.io/csb-fw5km3/static/js/main.ff9895ac.chunk.js"
+  },
+  {
+    "revision": "e6d8563d60d5b38cf10c",
+    "url": "https://gauripatole11.github.io/csb-fw5km3/static/js/runtime-main.c8db2cb1.js"
+  }
+]);
