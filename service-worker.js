3244eb25d35d62b87c37e47281a34821a51d74c7
Code Sandbox <hello@codesandbox.io>
2023-09-29 20:04:24 +0000

    Deployment from Codesandbox.

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https://gauripatole11.github.io/csb-fw5km3/precache-manifest.82bc72199153cee122cfc6c0e184f76b.js"
+  "https://.github.io/csb-fw5km3/precache-manifest.2a240cc467dbb9542df270e1b65ed1c3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://gauripatole11.github.io/csb-fw5km3/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://.github.io/csb-fw5km3/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
